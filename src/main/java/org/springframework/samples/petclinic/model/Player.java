cfca38c74f5eca2997d5b74dc6326ff518c112f2
Vasliska1 <v.lisitsina@gmail.com>
2020-07-02 11:53:11 +0300

    Add class Player

---
+++ src/main/java/org/springframework/samples/petclinic/model/Player.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.model;
+
+public class Player {
+	private String nickname;
+	private String registrationDate;
+
+	public String getPlayerResult(String nickname) {
+		return null;
+	}
+}
