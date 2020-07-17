f5755f6435e19392e0b0941bfda08117d16fe98d
Vasliska1 <v.lisitsina@gmail.com>
2020-07-17 22:08:20 +0300

    Create data

--- src/main/java/org/springframework/samples/petclinic/model/Player.java
+++ src/main/java/org/springframework/samples/petclinic/model/Player.java
@@ -1,10 +1,23 @@
 package org.springframework.samples.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Entity
+@Table(name = "player")
 public class Player {
+
+	@Column(name = "nickname")
+	@NotEmpty
 	private String nickname;
+
+	@Column(name = "registretion_date")
+	@NotEmpty
 	private LocalDateTime registrationDate;
 
 	public String getPlayerResult() {
