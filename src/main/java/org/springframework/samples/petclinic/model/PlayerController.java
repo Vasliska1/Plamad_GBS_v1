e2323e36b4a19f7eb547b146e76f025031d18d47
Vasliska1 <v.lisitsina@gmail.com>
2020-09-22 23:38:54 +0300

    add views add and find

--- src/main/java/org/springframework/samples/petclinic/model/PlayerController.java
+++ src/main/java/org/springframework/samples/petclinic/model/PlayerController.java
@@ -1,10 +1,19 @@
 package org.springframework.samples.petclinic.model;
 
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.owner.Pet;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class PlayerController {
+
+	private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "players/createPlayer";
 
 	final PlayerRepository player;
 
@@ -14,8 +23,32 @@ public class PlayerController {
 
 	@RequestMapping(value = "/player/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public String findById(@PathVariable("id") int id) {
-		return player.findById(id).getNickname() + " " + player.findById(id).getRegistrationDate();
+	public ModelAndView findById(@PathVariable("id") int id) {
+		ModelAndView mav = new ModelAndView("players/findPlayers");
+		Player player = this.player.findById(id);
+		mav.addObject(player);
+		return mav;
+	}
+
+	@GetMapping("/player/{id}/edit")
+	public String initUpdateOwnerForm(@PathVariable("id") int id, Model model) {
+		Player player = this.player.findById(id);
+		model.addAttribute(player);
+		return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+	}
+
+	@PostMapping("/player/{id}/edit")
+	public String processUpdateOwnerForm(@Valid Player player, BindingResult result,
+										 @PathVariable("id") int id) {
+		if (result.hasErrors()) {
+			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			player.setId(id);
+			this.player.save(player);
+			System.out.println(player.getNickname());
+			return "redirect:/player/{id}";
+		}
 	}
 
 }
