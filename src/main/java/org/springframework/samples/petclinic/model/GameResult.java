bd73f60a5fc482159106a3f0e4512768bc69ee4e
Vasliska1 <v.lisitsina@gmail.com>
2020-11-11 23:21:49 +0300

    change name date

--- src/main/java/org/springframework/samples/petclinic/model/GameResult.java
+++ src/main/java/org/springframework/samples/petclinic/model/GameResult.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class GameResult  extends BaseEntity {
 
 
-	@Column(name = "dateR")
+	@Column(name = "registration_date")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	private LocalDateTime date;
 
