42dbe5be0ec4846318984cc1a3406a1600c4a92a
Vasliska1 <v.lisitsina@gmail.com>
2020-11-22 15:51:57 +0300

    delete

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.owner.OwnerRepository;
 import org.springframework.samples.petclinic.owner.Pet;
 import org.springframework.samples.petclinic.owner.PetRepository;
